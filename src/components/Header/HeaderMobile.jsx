6baa0e91b54d61cf3d77b31c38ed0e88e9e942b2
kienpro <nguyenduckien1109@gmail.com>
2024-10-13 14:50:40 +0700

    update responsive for mobile

--- src/components/Header/HeaderMobile.jsx
+++ src/components/Header/HeaderMobile.jsx
@@ -6,7 +6,7 @@ import SearchContext from "../../context/headerContext";
 export default function HeaderMobile() {
   const { isSearching, setIsSearching } = useContext(SearchContext);
   const { isMenuOpen, setIsMenuOpen } = useContext(SearchContext);
-  console.log("ismenuopen",isMenuOpen);
+  console.log("ismenuopen", isMenuOpen);
   const inputRef = useRef(null);
 
   const handleSearchClick = () => {
@@ -28,7 +28,9 @@ export default function HeaderMobile() {
     setIsMenuOpen(false);
   };
   return (
-    <div className={`${isMenuOpen ? classes['header-open-menu'] :classes.header}`}>
+    <div
+      className={`${isMenuOpen ? classes["header-open-menu"] : classes.header}`}
+    >
       <div className={`${classes["header-logo"]}`}>
         <img
           src="https://img.freepik.com/premium-vector/restaurant-logo-design_1128391-17280.jpg"
@@ -37,55 +39,60 @@ export default function HeaderMobile() {
       </div>
 
       <ul className={`${classes.navbar}`}>
-        {!isMenuOpen ? <><div
-          className={`${
-            isSearching
-              ? classes["header-search-open"]
-              : classes["header-search"]
-          } `}
-        >
-          <button
-            className={`${isSearching ? classes.btnBack : classes.hidden}`}
-            onClick={handleBackSearch}
-          >
-            <i className="fa fa-arrow-left"></i>
-          </button>
-          <input
-            ref={inputRef}
-            className={`${
-              isSearching ? classes["search-open"] : classes["search-close"]
-            }`}
-            type="text"
-          />
-          <button
-            className={`${
-              isSearching
-                ? classes["button-search-open"]
-                : classes["button-search"]
-            }`}
-            onClick={handleSearchClick}
-          >
-            <i className="fa fa-search"></i>
-          </button>
-        </div>
-        <div className={classes["header-auth"]}>
-          <Link to={"/userinfo"}>
-            <button>
-              <i className="fa fa-user"></i>
-            </button>
-          </Link>
-        </div>
-        <div className={classes["header-fav"]}>
-          <button>
-            <i className="fa fa-heart"></i>
-          </button>
-        </div>
-        <div className={classes["header-cart"]}>
-          <button>
-            <i className="fa fa-shopping-cart"></i>
-          </button>
-        </div></> : <></>}
-        
+        {!isMenuOpen ? (
+          <>
+            <div
+              className={`${
+                isSearching
+                  ? classes["header-search-open"]
+                  : classes["header-search"]
+              } `}
+            >
+              <button
+                className={`${isSearching ? classes.btnBack : classes.hidden}`}
+                onClick={handleBackSearch}
+              >
+                <i className="fa fa-arrow-left"></i>
+              </button>
+              <input
+                ref={inputRef}
+                className={`${
+                  isSearching ? classes["search-open"] : classes["search-close"]
+                }`}
+                type="text"
+              />
+              <button
+                className={`${
+                  isSearching
+                    ? classes["button-search-open"]
+                    : classes["button-search"]
+                }`}
+                onClick={handleSearchClick}
+              >
+                <i className="fa fa-search"></i>
+              </button>
+            </div>
+            <div className={classes["header-auth"]}>
+              <Link to={"/userinfo"}>
+                <button>
+                  <i className="fa fa-user"></i>
+                </button>
+              </Link>
+            </div>
+            <div className={classes["header-fav"]}>
+              <button>
+                <i className="fa fa-heart"></i>
+              </button>
+            </div>
+            <div className={classes["header-cart"]}>
+              <button>
+                <i className="fa fa-shopping-cart"></i>
+              </button>
+            </div>
+          </>
+        ) : (
+          <></>
+        )}
 
         <div>
           {!isMenuOpen ? (
